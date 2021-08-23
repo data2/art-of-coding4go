9696f1b52edfc7dc15f204af79fc9ae95fdb0ab7
data2 <muskteerathos@gmail.com>
2021-08-23 16:06:42 +0800

    Update go-redis.go

--- go-redis.go
+++ go-redis.go
@@ -1,1 +1,19 @@
 package main 
+
+import (
+  "fmt"
+  "github.com/go-redis/redis"
+)
+
+func main(){
+  fmt.Println("golang start conn redis ")
+  
+  client := redis.NewClient(&redis.Options{
+        Addr: "192.168.8.200:6379",
+        Password: "123456",
+        DB: 0,
+    })
+  
+  pong, err := client.Pong().Result()
+  fmt.Println(pong, err)
+}
