fcd570c09e93d887fc2a02fc7d873da5ae21d0b2
data2 <muskteerathos@gmail.com>
2021-08-23 18:28:17 +0800

    Update go-redis.go

--- go-redis.go
+++ go-redis.go
@@ -9,11 +9,21 @@ func main(){
   fmt.Println("golang start conn redis ")
   
   client := redis.NewClient(&redis.Options{
-        Addr: "192.168.8.200:6379",
-        Password: "123456",
+        Addr: "20.26.52.89:6379",
+        //Password: "123456",
         DB: 0,
     })
   
   pong, err := client.Ping().Result()
   fmt.Println(pong, err)
+
+  err = client.Set("testKey","testGo",0).Err()
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Println("go redis setKey success")
+
+  value, err := client.Get("testKey").Result()
+  fmt.Println(value)
+
 }
